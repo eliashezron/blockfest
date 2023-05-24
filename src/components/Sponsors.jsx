08212f05fb612c5940fb769e9f2198bff599d0f3
Elias Hezron <eliashezron@Eliass-MacBook-Pro.local>
2023-05-24 12:51:06 +0300

    sponsors added

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,6 +1,11 @@
 import img4 from '../assets/img/innovationVillageLogo.png'
 import img3 from '../assets/img/worldcoinlogo.jpeg'
 import celologo from '../assets/celo-logo.png'
+import muda from '../assets/mudaLogo.png'
+import ngeni from '../assets/ngeni.png'
+import binusu from '../assets/binusu.png'
+import nileone from '../assets/n1Logopng.png'
+import motiv from '../assets/motivimg.jpeg'
 
 const Sponsors = () => {
   return (
@@ -12,7 +17,7 @@ const Sponsors = () => {
         <h1 className="mb-8 text-3xl font-bold text-center md:text-5xl">
           Sponsors
         </h1>
-        <div className="flex flex-row flex-wrap justify-evenly">
+        <div className="flex flex-row flex-wrap justify-evenly gap-2 justify-items-center">
           <a
             href="https://innovationvillage.co.ug/"
             target="_blank"
@@ -24,10 +29,13 @@ const Sponsors = () => {
               className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
             />
           </a>
-
-          <a href="https://celo.org/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://motiv.africa/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
-              src={celologo}
+              src={motiv}
               alt="sponsor"
               className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
             />
@@ -44,32 +52,53 @@ const Sponsors = () => {
               className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
             />
           </a>
-
-          {/* <img
-            src="https://cryptologos.cc/logos/tether-usdt-logo.png?v=024"
-            alt="sponsor"
-            className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
-          />
-          <img
-            src="https://cryptologos.cc/logos/tether-usdt-logo.png?v=024"
-            alt="sponsor"
-            className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
-          />
-          <img
-            src="https://cryptologos.cc/logos/tether-usdt-logo.png?v=024"
-            alt="sponsor"
-            className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
-          />
-          <img
-            src="https://cryptologos.cc/logos/tether-usdt-logo.png?v=024"
-            alt="sponsor"
-            className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
-          />
-          <img
-            src="https://cryptologos.cc/logos/tether-usdt-logo.png?v=024"
-            alt="sponsor"
-            className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
-          /> */}
+          <a href="https://celo.org/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={celologo}
+              alt="sponsor"
+              className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
+            />
+          </a>
+          <a href="https://ngeni.io/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={ngeni}
+              alt="sponsor"
+              className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
+            />
+          </a>
+          <a
+            href="https://muda.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={muda}
+              alt="sponsor"
+              className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
+            />
+          </a>
+          <a
+            href="https://binusu.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={binusu}
+              alt="sponsor"
+              className="w-28 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
+            />
+          </a>
+          <a
+            href="https://nileone.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={nileone}
+              alt="sponsor"
+              className="w-24 h-24 mx-1 mb-4 sm:w-32 sm:h-32 sm:mx-2"
+            />
+          </a>
         </div>
       </div>
     </section>
